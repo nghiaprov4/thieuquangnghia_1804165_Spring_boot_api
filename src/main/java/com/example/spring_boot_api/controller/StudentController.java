9c9d65de3b3fc97ec2de9eec68e541c32c7f5252
DESKTOP-41VA80C\IBM <thieuquangnghia2000@gmail.com>
2021-09-16 12:21:27 +0700

    nhanh 2

--- src/main/java/com/example/spring_boot_api/controller/StudentController.java
+++ src/main/java/com/example/spring_boot_api/controller/StudentController.java
@@ -2,13 +2,45 @@ package com.example.spring_boot_api.controller;
 
 import com.example.spring_boot_api.service.StudentService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/")
 public class StudentController {
     @Autowired
     private StudentService studentService;
+
+    @GetMapping("/students")
+    public List<com.example.springboot_main.entity.Student> getListStudent() {
+        System.out.println(studentService.findAll());
+        return studentService.findAll();
+    }
+    @GetMapping("/students/{id}")
+    public com.example.springboot_main.entity.Student getStudent(@PathVariable long id){
+        return studentService.findById(id);
+    }
+
+    @PostMapping("/students")
+    public com.example.springboot_main.entity.Student addNewStudent(@RequestBody com.example.springboot_main.entity.Student student){
+        student.setId(Long.valueOf(0));
+        studentService.saveStudent(student);
+        return student;
+    }
+
+    @PutMapping("/students")
+    public com.example.springboot_main.entity.Student updateStudent(@RequestBody com.example.springboot_main.entity.Student student){
+
+        studentService.saveStudent(student);
+        return student;
+    }
+
+    @DeleteMapping("/students/{id}")
+    public String deleteStudent(@PathVariable long id){
+        studentService.deleteById(Long.valueOf(id));
+        return "Deleted student "+ id;
+    }
+
 
 }
